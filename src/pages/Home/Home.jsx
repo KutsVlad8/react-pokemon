782d56483162e07dabb760d821bb3fa5a3c1e4bf
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2025-01-26 21:38:40 +0200

    собрал все на странице Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,54 @@
+// Базовые импорты
 import React from 'react';
+import { useEffect, useState } from 'react';
 
-const Home = () => (
-  <div>
-    <h1>Главная страница</h1>
-    <p>Добро пожаловать на наш сайт!</p>
-  </div>
-);
+// Запрос API
+import { fetchRandomPokemons } from '../../api/pokemonApi';
+
+// Компоненты
+import MainTitle from '../../components/MainTitle/MainTitle';
+import Title from '../../components/Title/Title';
+import PokemonList from '../../components/PokemonList/PokemonList';
+
+// Стили
+import { Text } from './Home.styled';
+
+const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const getRandomPokemons = async () => {
+      try {
+        const data = await fetchRandomPokemons();
+        setPokemons(data);
+      } catch (error) {
+        console.error('Error fetching pokemons:', error);
+      }
+    };
+
+    getRandomPokemons();
+  }, []);
+
+  console.log(pokemons);
+
+  return (
+    <>
+      <MainTitle text="Поринь у світ дивовижних створінь " />
+
+      <Text>
+        Покемоны — это фантастические существа, которые обитают в мире, полном
+        магии и волшебства.Путешествия покемонов полны удивительных встреч и
+        волнующих событий. Вместе с тренерами они сражаются, развиваются, и учат
+        нас важным жизненным урокам,таким как дружба, смелость,
+        ответственность,честность и любовь к природе. Они могут быть разных
+        типов: огненные, водные, ледяные и многие другие! Многие покемоны могут
+        эволюционировать и становиться ещё сильнее!
+      </Text>
+      <Title text="Спіймай їх усіх!" />
+
+      <PokemonList pokemonData={pokemons} />
+    </>
+  );
+};
 
 export default Home;
